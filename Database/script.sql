1ba234ab30dafbaee8c1fa951c05b51f3d00b654
nini1504 <nicolly.luz@ufu.br>
2026-03-05 11:45:32 -0300

    insert on bd ok

---
+++ Database/script.sql
@@ -0,0 +1,26 @@
+CREATE TABLE cargos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    ascensao BOOLEAN DEFAULT FALSE
+);
+
+
+CREATE TABLE salarios (
+    id_cargo INT NOT NULL REFERENCES cargos(id) ON DELETE CASCADE,
+    salario_junior DOUBLE PRECISION,
+    salario_pleno DOUBLE PRECISION,
+    salario_senior DOUBLE PRECISION,
+    ano INT,
+    PRIMARY KEY (id_cargo, ano)
+);
+
+
+ALTER TABLE salarios
+ADD COLUMN media DOUBLE PRECISION;
+
+INSERT INTO cargos (id, nome, ascensao)
+VALUES 
+(1, 'Administrador de Banco de Dados', false),
+(2, 'Administrador de Rede', false);
+
+DROP table cargos;
