b5416493ecfbea1c7f5f1da7087c76b79ebbb8a6
Mariana <mariana.souza2@ufu.br>
2026-03-16 14:23:41 -0300

    Página empresa estruturada para consumir a API

--- Frontend/src/pages/Empresas/Empresas.jsx
+++ Frontend/src/pages/Empresas/Empresas.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Empresas.css';
 
 const Empresas = () => {
+
+  const [empresas, setEmpresas] = useState([]);
+
+  useEffect(() => {
+
+    fetch("http://localhost:8000/empresas")//quando existir conexão com a api
+      .then(res => res.json())
+      .then(data => setEmpresas(data))
+      .catch(err => console.error("Erro ao buscar empresas:", err))
+
+  }, []);
+
   return (
-    <div className="empresas-placeholder">
+    <div className="empresas-container">
+
       <h1>Página de Empresas</h1>
-      <p>Dados aqui </p>
+
+      <div className="empresas-grid">
+
+        {empresas.map((empresa) => (
+
+          <div className="empresa-card" key={empresa.id}>
+
+            <img
+              src={empresa.imagem}
+              alt={empresa.nome}
+              className="empresa-logo"
+            />
+
+            <h3>{empresa.nome}</h3>
+
+            <a href={empresa.linkedin} target="_blank" rel="noopener noreferrer">
+              Ver no Linkedin
+            </a>
+
+          </div>
+
+        ))}
+
+      </div>
+
     </div>
   );
 };
